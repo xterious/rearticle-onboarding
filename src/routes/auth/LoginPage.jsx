88b1c671269dffc360547fa8ff015413e732fa5b
Destroyer <mukunthan.1255@gmail.com>
2024-03-01 14:56:04 +0530

    Building SignUp component

--- src/routes/auth/LoginPage.jsx
+++ src/routes/auth/LoginPage.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/ui/Logo.svg";
 import InputFieldComponent from "../../components/ui/InputFieldComponent";
 import PrimaryButtonComponent from "../../components/ui/PrimaryButtonComponent";
 
 const LoginPage = () => {
+  const [continued, setContinued] = useState(false);
   return (
     <div className="flex justify-center items-center h-svh relative w-full">
       <div className="flex gapy-2 flex-col w-full md:w-1/2 lg:w-1/4 px-4">
@@ -12,20 +13,15 @@ const LoginPage = () => {
         </div>
         <div className="flex gap-y-6 flex-col">
           <h3 className="text-3xl text-[#424242] text-center font-bold">
-            Tell us about you
+            Create your account
           </h3>
-          <InputFieldComponent placeholder="Your Name"></InputFieldComponent>
-          <InputFieldComponent placeholder="Name of the Institution"></InputFieldComponent>
+          <InputFieldComponent placeholder="Email address"></InputFieldComponent>
+          <PrimaryButtonComponent name="Continue"></PrimaryButtonComponent>
           <p className="text-[#424242b3] text-xs/[22px] font-medium cursor-default text-center">
-            By clicking “Agree”, you agree to our
-            <a href="#" className="text-blue-600 underline ml-1">
-              Terms
+            Don't have an account ?
+            <a href="#" className="text-blue-600 ml-1">
+              Sign up
             </a>
-            . Our <br />
-            <a href="#" className="text-blue-600 underline mr-1">
-              Privacy Policy
-            </a>
-            explains how we process your data
           </p>
           <div className="flex w-full rounded-lg">
             <PrimaryButtonComponent name="Agree"></PrimaryButtonComponent>
