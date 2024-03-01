ebf62f9d8592b48ec4c5feba88378d687be37e9f
Destroyer <mukunthan.1255@gmail.com>
2024-03-01 14:01:43 +0530

    Made responsive for Mobile Screens

--- src/routes/auth/LoginPage.jsx
+++ src/routes/auth/LoginPage.jsx
@@ -4,23 +4,23 @@ import Logo from "../../assets/ui/Logo.svg";
 const LoginPage = () => {
   return (
     <div className="flex justify-center items-center h-svh relative w-full">
-      <div className="flex gapy-2 flex-col items-center justify-center">
+      <div className="flex gapy-2 flex-col w-full md:w-1/2 lg:w-1/4 px-4">
         <img className="w-18" src={Logo} alt="Rearticle Logo" />
-        <div className="flex gap-y-6 flex-col items-center justify-center">
-          <h3 className="text-3xl text-[#424242] font-bold">
+        <div className="flex gap-y-6 flex-col">
+          <h3 className="text-3xl text-[#424242] text-center font-bold">
             Tell us about you
           </h3>
           <input
             type="text"
-            class="border border-gray-300 rounded-md pl-4 pr-20 py-2 w-full focus:outline-none focus:border-blue-500 "
+            className="border border-gray-300 rounded-md pl-4 pr-20 py-2 focus:outline-none focus:border-blue-500"
             placeholder="Your Name"
-          ></input>
+          />
           <input
             type="text"
-            class="border border-gray-300 rounded-md pl-4 pr-20 py-2 w-full focus:outline-none focus:border-blue-500"
+            class="border border-gray-300 rounded-md pl-4 pr-20 py-2 focus:outline-none focus:border-blue-500"
             placeholder="Name of the Institution"
-          ></input>
-          <p className="text-[#424242b3] text-xs/[22px] font-medium cursor-default">
+          />
+          <p className="text-[#424242b3] text-xs/[22px] font-medium cursor-default text-center">
             By clicking “Agree”, you agree to our
             <a href="#" className="text-blue-600 underline ml-1">
               Terms
